3ba6583a9e9340d39355a6a6d776c1920b371114
Iván Villagrasa Pérez <56278092+ivvil@users.noreply.github.com>
2025-01-13 14:07:25 +0100

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Trial dev flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    utils,
+  }:
+    utils.lib.eachDefaultSystem
+    (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in {
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [
+          gprbuild
+          gnat
+        ];
+      };
+    });
+}
