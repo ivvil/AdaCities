d86b1f25c003b1e99d636417f6e0a5991e794236
Iván Villagrasa <ivvil412@gmail.com>
2025-01-14 10:45:35 +0100

    Add test code

--- src/adacities.adb
+++ src/adacities.adb
@@ -1,6 +1,12 @@
-with Ada.Text_Io;	
-		
+with Ada.Text_IO;
+
 procedure Adacities is
+   function Site_Info (Site : String) return String is
+      Url : String := "https://neocities.org/api/info?sitename=" & Site;
+   begin
+      --  return Util.Http.Get(Url).Get_Body;
+      return Url;
+   end Site_Info;
 begin
-	Ada.Text_IO.Put_Line ("Hello, world!");	
+   Ada.Text_IO.Put_Line (Site_Info ("hii"));
 end Adacities;
